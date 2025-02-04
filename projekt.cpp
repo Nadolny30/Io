bb2b22edc6629917df58431717bf0203a1a9cda9
Kacper Nadolny <323014@stud.umk.pl>
2025-02-04 19:12:52 +0100

    Dodano funckje sprawdzajaca liczby pierwsze

--- projekt.cpp
+++ projekt.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
+
+bool czy_pierwsza(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+
 int main()
 {
 	int liczba;
 	cin>>liczba;
-	cout<<liczba<<endl;
+	cout << liczba << " jest " << (czy_pierwsza(liczba) ? "pierwsza" : "zlozona") << endl;
 	return 0;
 }

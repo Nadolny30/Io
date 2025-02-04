4b36af0a89de27cd6472d8d64b7b346b2bb00367
Kacper Nadolny <323014@stud.umk.pl>
2025-02-04 19:14:39 +0100

    Dodano sprawdzenie, czy liczba jest dodatnia

--- projekt.cpp
+++ projekt.cpp
@@ -4,6 +4,10 @@ int main()
 {
 	int liczba;
 	cin>>liczba;
+	if (liczba <= 0) {
+        cout << "Blad: liczba musi byc dodatnia" << endl;
+        return 1;
+   	 }
 	cout<<liczba<<endl;
 	return 0;
 }
